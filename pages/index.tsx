e54fea9d8e5107cdb2f2dd831a9446af3a68f066
vmiguellima <vitormiguel_1999@hotmail.com>
2021-08-13 16:24:31 +0100

    Implementation with svg icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 
+import CodeIcon from '@/assets/svgs/code.svg';
+
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen min-w-full flex items-center justify-center">
       <div>
+        <CodeIcon />
         <h1 className="font-extrabold w-full text-center text-4xl">Just Me</h1>
         <ul className="mt-4 space-y-4 list-none text-center list-inside text-gray-400">
           <li>TailwindCSS</li>
